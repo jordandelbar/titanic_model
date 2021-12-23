135f0535138a506e2a8c28f10692618ee2687492
jordandelbar <jordandelbar@gmail.com>
2021-12-23 21:51:20 +0100

    test on circleci

--- scripts/get_datasets.sh
+++ scripts/get_datasets.sh
@@ -1,3 +1,3 @@
-kaggle competitions download -c titanic -p titanic_model/titanic_model/datasets/
+kaggle competitions download -c titanic -p ~/titanic/titanic_model/datasets/
 unzip titanic.zip
 rm titanic.zip
