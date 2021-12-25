d6e033c0e16659cdda4e7f3e9879bbeaf617cccc
jordandelbar <jordandelbar@gmail.com>
2021-12-26 00:04:17 +0100

    test with get_datasets.sh script

--- scripts/get_datasets.sh
+++ scripts/get_datasets.sh
@@ -1,3 +1,4 @@
-kaggle competitions download -c titanic -p ~/titanic/titanic_model/datasets/
-unzip titanic.zip
-rm titanic.zip
+$path = ~/titanic/titanic_model/datasets/
+kaggle competitions download -c titanic -p $path
+unzip $path/titanic.zip
+rm $path/titanic.zip
