fb84535748842385545db085dd32859f41d3fdce
jordandelbar <jordandelbar@gmail.com>
2021-12-26 00:10:25 +0100

    changed pipeline order

--- scripts/get_datasets.sh
+++ scripts/get_datasets.sh
@@ -1,4 +1,3 @@
-path = "~/titanic/titanic_model/datasets/"
-kaggle competitions download -c titanic -p $path
-unzip $path/titanic.zip
-rm $path/titanic.zip
+kaggle competitions download -c titanic -p ~/titanic/titanic_model/datasets/
+unzip ~/titanic/titanic_model/datasets//titanic.zip
+rm ~/titanic/titanic_model/datasets//titanic.zip
