422a7b5ec6ed2ab118a555d55d695331df157bda
jordandelbar <jordandelbar@gmail.com>
2021-12-30 23:33:47 +0100

    test the tag pipeline

--- scripts/get_datasets.sh
+++ scripts/get_datasets.sh
@@ -1,3 +1,3 @@
-kaggle competitions download -c titanic -p ~/titanic/titanic_model/datasets/
-unzip ~/titanic/titanic_model/datasets/titanic.zip -d ~/titanic/titanic_model/datasets/
-rm ~/titanic/titanic_model/datasets/titanic.zip
+kaggle competitions download -c titanic -p ~/titanic_model/titanic_model/datasets/
+unzip ~/titanic_model/titanic_model/datasets/titanic.zip -d ~/titanic_model/titanic_model/datasets/
+rm ~/titanic_model/titanic_model/datasets/titanic.zip
